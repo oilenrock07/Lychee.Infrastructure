5e2444ebe537a48fb5bf6a78065a8298ee89fcf6
CAWI\User <cawicaancornelio@gmail.com>
2020-02-06 15:54:50 +1300

    Added setting repository as default repository. Assuming all the other projects will have setting table

---
+++ Lychee.Infrastructure/Lychee.Infrastructure/Entities/Setting.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lychee.Infrastructure.Entities
+{
+    public class Setting
+    {
+        [Key]
+        public int SettingId { get; set; }
+
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
---
+++ Lychee.Infrastructure/Lychee.Infrastructure/Repositories/SettingRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lychee.Infrastructure.Entities;
+using Lychee.Infrastructure.Interfaces;
+
+namespace Lychee.Infrastructure.Repositories
+{
+    public class SettingRepository : ISettingRepository
+    {
+        private readonly IRepository<Setting> _settingRepository;
+        protected virtual List<Setting> _settings { get; set; }
+
+        public SettingRepository(IRepository<Setting> settingRepository)
+        {
+            _settingRepository = settingRepository;
+        }
+
+        public virtual ICollection<Setting> GetAllSettings()
+        {
+            if (_settings != null) return _settings;
+
+            _settings = _settingRepository.GetAll().ToList();
+            return _settings;
+        }
+
+        public virtual Setting GetSetting(string key)
+        {
+            if (_settings == null)
+                GetAllSettings();
+
+            return _settings.FirstOrDefault(x => x.Key == key);
+        }
+
+        public virtual T GetSettingValue<T>(string key)
+        {
+            if (_settings == null)
+                GetAllSettings();
+
+            var setting = GetSetting(key);
+            if (setting == null)
+                return default;
+
+            return (T) Convert.ChangeType(setting.Value, typeof(T));
+        }
+    }
+}
